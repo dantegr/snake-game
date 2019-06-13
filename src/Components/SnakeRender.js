76e467ef5e0a18f5e2feb206d7e9fb3e2135c95a
Pantelis <tampantelis@gmail.com>
2019-06-13 09:55:08 +0300

    initial commit

---
+++ src/Components/SnakeRender.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SnakeRender = () => {
+  return (
+    <div className="SnakeRender">
+      <p>this is where rendering stuff goes</p>
+    </div>
+  );
+}
+
+export default SnakeRender;
