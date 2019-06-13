0671848c3beea28abe2848ca9895e297969a6958
Pantelis <tampantelis@gmail.com>
2019-06-13 11:44:20 +0300

    added the snake component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import SnakeRender from './Components/SnakeRender';
 import './assets/app.scss';
 
--- src/Components/SnakeRender.js
+++ src/Components/SnakeRender.js
@@ -1,18 +1,32 @@
 import React from 'react';
+import Snake from './Snake';
 
-const SnakeRender = () => {
+
+export default class SnakeRender extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      snakeDots: [
+        [0,0],
+        [2,0]
+      ]
+    }
+  }
 
 
-  return (
-    <div className="SnakeRender">
-    <div className="container">
-      <h1>Snake Game</h1>
-      <canvas width="768" height="448"></canvas>
-    </div>
+  render() {
+    return (
+      <div className="SnakeRender">
       
-      
-    </div>
-  );
+        <h1>Snake Game</h1>
+        <div className="game-area">
+          <Snake snakeDots={this.state.snakeDots} />
+        </div>
+  
+        
+      </div>
+    );
+
+  }
 }
 
-export default SnakeRender;
